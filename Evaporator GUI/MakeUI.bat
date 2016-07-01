4ac6fea341d1784d1aa88de72d545e125ad1665c
mserlin <marecserlin@gmail.com>
2016-07-01 14:52:14 -0700

    Add files via upload

--- Evaporator GUI/MakeUI.bat
+++ Evaporator GUI/MakeUI.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 call activate Python27
 
-pyuic4 EvaporatorUIv3.ui -o EvaporatorUI.py & pyuic4 dataCollectorUIv3.ui -o dataCollectorUI.py
+pyuic4 EvaporatorUI.ui -o EvaporatorUI.py & pyuic4 dataCollectorUI.ui -o dataCollectorUI.py
