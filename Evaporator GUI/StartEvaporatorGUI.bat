3e423a6c142b084e6eff672d90bb4c8a185cdfa2
ashragai <ashragai@umail.ucsb.edu>
2018-07-08 14:37:45 -0700

    Add files via upload

--- Evaporator GUI/StartEvaporatorGUI.bat
+++ Evaporator GUI/StartEvaporatorGUI.bat
@@ -1,22 +1,22 @@
 start labrad
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\serial_device_manager.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\serial_device_manager.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\serial_server.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\serial_server.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\data_vault.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\data_vault.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\FTM_2400.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\FTM_2400.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\Turbo450.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\Turbo450.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\RVC_300.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\RVC_300.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\StepperBoxEvaporator.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\StepperBoxEvaporator.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\TDK_GEN10-240.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\TDK_GEN10-240.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\ValveRelayBoxEvaporator.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator Servers\ValveRelayBoxEvaporator.py
 timeout 3
-start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator GUI\EvaporatorController.py
+start python "Z:\_personal\Avi\Scripts\LabRAD-Evaporator-master\Evaporator GUI\EvaporatorController.py
 timeout 3
