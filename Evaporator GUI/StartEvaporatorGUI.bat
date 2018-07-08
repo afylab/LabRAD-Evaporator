326f48bdd870eea4e4c8d8beaa71624c70c9d9d6
ashragai <ashragai@umail.ucsb.edu>
2018-07-08 14:30:41 -0700

    Add files via upload

---
+++ Evaporator GUI/StartEvaporatorGUI.bat
@@ -0,0 +1,22 @@
+start labrad
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\serial_device_manager.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\serial_server.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\data_vault.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\FTM_2400.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\Turbo450.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\RVC_300.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\StepperBoxEvaporator.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\TDK_GEN10-240.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator Servers\ValveRelayBoxEvaporator.py
+timeout 3
+start python "D:\Users\LordKelvin\Workspace\Software\GUIs\LabRAD-Evaporator-master\Evaporator GUI\EvaporatorController.py
+timeout 3
